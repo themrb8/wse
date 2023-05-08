a4d83df9b796f40094966d0ab23f02115bdeb218
themrb8 <officialroman786@gmail.com>
2023-05-08 19:29:34 +0600

    first commit

---
+++ module-2/main.js
@@ -0,0 +1,40 @@
+console.log('hello world')
+var Mohammad = "roman"
+var mrb = 88945894.332
+var isCompleted = true
+
+
+var day  = 'thuresday'
+switch (day) {
+    case 'saturday':
+    console.log('this is saturday');
+    break;
+
+    case 'sunday':
+    console.log('this is sunday');
+    break;
+
+    case 'monday':
+    console.log('this is monday');
+    break;
+
+    case 'thuesday':
+    console.log('this is thuesday');
+    break;
+
+    case 'wednesday':
+    console.log('this is wednesday');
+    break;
+
+    case 'thursday':
+    console.log('this is thursday');
+    break;
+
+    case 'friday':
+    console.log('this is friday');
+    break;
+
+    default:
+        console.log('This is ' + day + ' day not exist in the world');
+        break;
+}
