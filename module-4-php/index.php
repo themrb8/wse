55f1eb2ff8e683eebd68ecc37a05f55065def9eb
themrb8 <officialroman786@gmail.com>
2023-05-15 10:59:11 +0600

    update directories

---
+++ module-4-php/index.php
@@ -0,0 +1,85 @@
+<?php
+// function name ($f, $l) {
+//     echo "Hello, " .$f .' '.$l;
+// }
+// name ('Mohammad', 'R.');
+// $sampleArray = [
+//     'name', 
+//     'phone',
+//     'email',
+//     'village',
+//     'city',
+//     'district'
+// ];
+// var_dump($sampleArray);
+
+// $name = '33';
+// switch ($name) {
+//     case 'roman':
+//         echo 'hello';
+//         break;
+//     case 'taskin':
+//         echo 'i am the taskin';
+//         break;
+//     default: {
+//         echo 'the default';
+//     }
+// }
+
+
+//associative array
+// $aArray = [
+//     'name' => 'Mohammad R.',
+//     'city' => 'Pabna',
+//     'pincode' => 6600,
+//     'district' => 'Rajshahi',
+//     'phone' => 78978978947797,
+// ];
+// echo 'Hello, I am '. $aArray['name']. ' Live at '. $aArray['city']. ' and pincode is ' .$aArray['pincode']. ' in '. $aArray['district']. '. Here is my phone number: '. $aArray['phone'];
+// echo '<br>';
+// echo '<pre>';
+// var_dump($aArray);
+// echo '</pre>';
+
+
+
+
+// //multi-dimensional array
+// $discord = [
+//     'server_name' => 'shikhun.net',
+//     'channels' => [
+//         'information' => ['welcome-and-rules', 'notice-board', 'jobs'],
+//         'learning resources' => ['english', 'php', 'css', 'wordpress'],
+//         'text-channels' => [
+//             'practices' => [
+//                 'users' => [
+//                     'roman' => 'php module 4 - multidimensional array',
+//                     'raihan' => 'php basic',
+//                     'rumman' => 'html css'
+//                 ],
+//             ],
+//             'help' => [
+//                 'users' => [
+//                     'roman' => 'php module 4 - multidimensional array',
+//                     'raihan' => 'php basic',
+//                     'rumman' => 'html css'
+//                 ],
+//             ],
+//             'off-topics' => [
+//                 'users' => [
+//                     'roman' => 'php module 4 - multidimensional array',
+//                     'raihan' => 'php basic',
+//                     'rumman' => 'html css'
+//                 ],
+//             ],
+//             'resendential students' => [
+//                 'users' => ['rouf', 'nazmul', 'akash', 'shakin', 'raihan', 'rakib', 'rohan'],
+//             ],
+//         ],
+//         'wordpress support engineer' => ['wse', 'wse mentor student exchange'],
+//     ],
+// ];
+// echo $discord['server_name'];
+// echo '<pre>';
+// var_dump($discord['channels']);
+// echo '</pre>';
